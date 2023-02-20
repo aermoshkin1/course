d952f9d1c17ae78f5283aaf6627d4c37c46752e7
aerm <ermlexa@gmail.com>
2023-02-20 20:42:48 +0300

    commit confest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-
+#d
 
 
 @pytest.fixture(scope="function")
